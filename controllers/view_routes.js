c89982ea18fb9b84ef4d5e9fdb97da0539597609
andrew.opitz222@gmail.com <andrew.opitz@yahoo.com>
2023-12-08 15:47:12 -0500

    view routes mostly done

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -4,8 +4,44 @@ const Post = require('../models/Post')
 
 const { loggedIn, isAuthed, authenticate } = require('./helpers')
 
-router.get('/home', (req, res) => {
-    res.render('home', { pageTitle: 'dashboard'})
+router.get('/', (req, res) => {
+    res.render('landing', { pageTitle: 'Landing'})
+})
+
+router.get('/register', loggedIn, authenticate, (req, res) => {
+    res.render('register-form', {
+        error: req.session.errors,
+        user: req.user
+    })
+
+    req.session.errors = []
+})
+
+router.get('/login', loggedIn, authenticate, (req, res) => {
+    res.render('login-form', {
+        errors: req.session.errors,
+        user: req.user
+    })
+
+    req.session.errors = []
+})
+
+router.get('/profile', loggedIn, authenticate, (req, res) => {
+    res.render('profile', {
+        errors: req.session.errors,
+        user: req.user
+    })
+
+    req.session.errors = []
+})
+
+router.get('/dashboard', loggedIn, authenticate, (req, res) => {
+    res.render('dashboard', {
+        errors: req.session.errors,
+        user: req.user
+    })
+
+    req.session.errors = []
 })
 
 
